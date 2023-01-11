654329c445185ef70f9b8a1eb8bac49b2e15d004
Mbirepinte <mickael.birepinte@gmail.com>
2023-01-11 23:28:05 +0100

    add apiUrl

--- frontend/apis/userApi.js
+++ frontend/apis/userApi.js
@@ -4,6 +4,6 @@ const apiUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const loginUser = (email, password) => {
   return axios
-    .post("user/login", { email, password }, { withCredentials: true })
+    .post(apiUrl + "user/login", { email, password }, { withCredentials: true })
     .then((response) => response.data);
 };
