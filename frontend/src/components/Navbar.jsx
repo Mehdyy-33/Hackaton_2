f9eaed39e7f50b68666fb612d1a73d334ae7642e
mehdy <mehdy33852@gmail.com>
2023-01-12 18:28:37 +0100

    pull dev

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+// import React from "react";
+
+// import "../services/Styles/navbar.css";
+
+// function Navbar() {
+//   const [isOpen, setIsOpen] = React.useState(false);
+
+//   return (
+//     <div className="smartfleetlogoblue">
+//       <img
+//         className="logoheight"
+//         src="../src/assets/images/Logo-smartfleetbleu2.png"
+//         alt="smartfleetlogo"
+//       />
+//       <div className="header-select">
+//         <div className="navbarmenu">My trips</div>
+//         {isOpen && (
+//           <div>
+//             <ul className="menuicon">
+//               <li> Connexion</li>
+//               <li>Register</li>
+//             </ul>
+//           </div>
+//         )}
+//       </div>
+
+//       <img
+//         onClick={() => setIsOpen(!isOpen)}
+//         className="usericon"
+//         src="../src/assets/images/usericon.png"
+//         alt="usericon"
+//       />
+//     </div>
+//   );
+// }
+
+// export default Navbar;
