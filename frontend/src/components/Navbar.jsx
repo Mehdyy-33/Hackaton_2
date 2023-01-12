507329059980754158af4e35a69264f43f2c15b4
mehdy <mehdy33852@gmail.com>
2023-01-12 18:39:47 +0100

    push ToRent

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,37 +1,37 @@
-// import React from "react";
+import React from "react";
 
-// import "../services/Styles/navbar.css";
+import "../assets/css/navbar.css";
 
-// function Navbar() {
-//   const [isOpen, setIsOpen] = React.useState(false);
+function Navbar() {
+  const [isOpen, setIsOpen] = React.useState(false);
 
-//   return (
-//     <div className="smartfleetlogoblue">
-//       <img
-//         className="logoheight"
-//         src="../src/assets/images/Logo-smartfleetbleu2.png"
-//         alt="smartfleetlogo"
-//       />
-//       <div className="header-select">
-//         <div className="navbarmenu">My trips</div>
-//         {isOpen && (
-//           <div>
-//             <ul className="menuicon">
-//               <li> Connexion</li>
-//               <li>Register</li>
-//             </ul>
-//           </div>
-//         )}
-//       </div>
+  return (
+    <div className="smartfleetlogoblue">
+      <img
+        className="logoheight"
+        src="../src/assets/images/Logo-smartfleetbleu2.png"
+        alt="smartfleetlogo"
+      />
+      <div className="header-select">
+        <div className="navbarmenu">My trips</div>
+        {isOpen && (
+          <div>
+            <ul className="menuicon">
+              <li> Connexion</li>
+              <li>Register</li>
+            </ul>
+          </div>
+        )}
+      </div>
 
-//       <img
-//         onClick={() => setIsOpen(!isOpen)}
-//         className="usericon"
-//         src="../src/assets/images/usericon.png"
-//         alt="usericon"
-//       />
-//     </div>
-//   );
-// }
+      <img
+        onClick={() => setIsOpen(!isOpen)}
+        className="usericon"
+        src="../src/assets/images/usericon.png"
+        alt="usericon"
+      />
+    </div>
+  );
+}
 
-// export default Navbar;
+export default Navbar;
--- frontend/src/pages/ToRent.jsx
+++ frontend/src/pages/ToRent.jsx
@@ -52,46 +52,6 @@ function ToRent() {
           <li>Informations sur la location</li>
           <li>CGV</li>
           <li>Politique de confidentialité</li>
-          <div className="block-3">
-            <p>Départ</p>
-            <p>Bordeaux Aéroport</p>
-            <p>13.01.2023 à 12:00 heures</p>
-            <br />
-            <br />
-            <p>Retour</p>
-            <p>Bordeaux Gare Saint-Jean TGV </p>
-            <p>16.01.2023 à 09:00 heures </p>
-            <br />
-            <br />
-            <p>Période de location</p>
-            <p>3 jours </p>
-            <br />
-            <br />
-            <ul>Équipement du véhicule</ul>
-            <li>climatisation</li>
-            <li>Boîte automatique</li>
-            <li>4 Portes</li>
-            <br />
-            <br />
-            <ul>Capacité</ul>
-            <li>5 Personnes</li>
-            <li>1 Valise</li>
-            <li>2 Bagages à mai</li>
-            <br />
-            <br />
-            <p>Age minimum du conducteur</p>
-            <p>18 ans</p>
-            <br />
-            <br />
-            <p>Durée minimum de détention du permis de conduire</p>
-            <p>5 année(s)</p>
-            <br />
-            <br />
-            <ul>Conditions générales</ul>
-            <li>Informations sur la location</li>
-            <li>CGV</li>
-            <li>Politique de confidentialité</li>
-          </div>
         </div>
       </div>
     </div>
